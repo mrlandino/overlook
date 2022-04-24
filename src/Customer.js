f695e158d96bc3016ed7cfd9f3b557655d525b45
Ross Landino <mrlandino@gmail.com>
2022-04-23 23:12:39 -0600

    Add method that adds all bookings information to myBookings

--- src/Customer.js
+++ src/Customer.js
@@ -1,16 +1,55 @@
 class Customer {
-  constructor(customer, allBookings){
+  constructor(customer, allBookings, allRooms){
     this.name = customer.name;
     this.id = customer.id;
     this.myBookings = [];
-    this.allBookings = allbookings;
+    this.allBookings = allBookings;
+    this.allRooms = allRooms;
     this.totalSpent = 0;
     this.numBookings = 0;
     this.username = `customer${this.id}`;
     this.password = "overlook2021";
   }
 
-  //function that creates myBookings
+  updateMyBookings() {
+    // this.myBookings = [];
+    // console.log(this.allBookings.bookings);
+    this.allBookings.bookings.filter(booking => {
+      if(booking.userID === this.id) {
+        this.myBookings.push(booking);
+      }
+    });
+    this.numBookings = this.myBookings.length;
+
+    let moneySpent = this.myBookings.reduce((total, booking) => {
+      this.allRooms.allRooms.rooms.forEach(room => {
+          total += room.costPerNight;
+      })
+      return total;
+    }, 0)
+
+    this.totalSpent = (Math.round(moneySpent * Math.pow(10,2)) / Math.pow(10,2)).toFixed(2);
+
+    this.myBookings.map(booking => {
+      if(!booking.roomCost) {
+        this.allRooms.allRooms.rooms.forEach(room => {
+          if(room.number === booking.roomNumber){
+          booking.roomCost = room.costPerNight;
+          booking.bedSize = room.bedSize;
+          if(room.bidet) {
+            booking.bidet = "yes";
+          } else if (!room.bidet) {
+            booking.bidet = "no";
+          };
+          booking.numBeds = room.numBeds;
+          booking.roomType = room.roomType;
+          }
+        });
+      };
+    });
+  }
+
+  
   //function that creates totalSpent by customer
   //function that creates numBookings by customer
 }
