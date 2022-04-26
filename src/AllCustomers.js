c1681646ac9a09e202a6782c533f76a417dad4e5
Ross Landino <mrlandino@gmail.com>
2022-04-26 14:23:27 -0600

    Refactor to remove unused code

--- src/AllCustomers.js
+++ src/AllCustomers.js
@@ -1,10 +1,7 @@
 class AllCustomers {
   constructor(allCustomers) {
     this.allCustomers = allCustomers;
-    this.totalCustomers = 0; //allcustomer.length?
   }
-
-  //function to calculate the total number of customers
 }
 
 export default AllCustomers;
