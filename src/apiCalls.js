82f2c464c7b87917c7ee3aea73e43f804e4e489b
Ross Landino <mrlandino@gmail.com>
2022-04-25 19:15:18 -0600

    Add fetch request to POST to database

--- src/apiCalls.js
+++ src/apiCalls.js
@@ -17,8 +17,27 @@ const getAllData = (id) => {
   ]);
 };
 
+const postBooking = (bookingRequestObj) => {
+  console.log(bookingRequestObj)
+  return fetch("http://localhost:3001/api/v1/bookings", {
+    method: 'POST',
+    headers: {
+      'Content-Type': 'application/json'
+    },
+    body: JSON.stringify(bookingRequestObj),
+  })
+  .then(response => checkForError(response))
 
+}
+
+const checkForError = (response) => {
+  if (!response.ok) {
+    throw new Error("Please make sure that all fields are filled out and that the POST path is correct");
+  } else {
+    return response.json()
+  }
+}
 //need to post new data here with new functions
 //error handling function?
 
-export { getAllData, getSpecificData };
+export { getAllData, getSpecificData, postBooking };
