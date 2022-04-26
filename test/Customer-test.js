3abee3025a5233c72196e4f7c70c3ca937bab810
Ross Landino <mrlandino@gmail.com>
2022-04-26 14:24:31 -0600

    Complete customer testing

--- test/Customer-test.js
+++ test/Customer-test.js
@@ -2,17 +2,54 @@ import chai from 'chai';
 const expect = chai.expect;
 import sampleData from './testData';
 import Customer from '../src/Customer';
+import BookingsRepo from '../src/BookingsRepo';
+import AllCustomers from '../src/AllCustomers';
 
 describe('Customer Class', function() {
+  let data, allBookings, newCustomer, allCustomers, newCustomer2
 
   beforeEach(() => {
-  //   card1 = new Card(1, 'What allows you to define a set of related information using key-value pairs?', ['object', 'array', 'function'], 'object');
-  //   card2 = new Card(2, "What is a comma-separated list of related values?", ["array", "object", "function"],"array");
-  //   deck = new Deck([card1, card2]);
-  // });
-})
+    data = sampleData;
+    allBookings = new BookingsRepo(data, data);
+    allCustomers = new AllCustomers(data.customers)
 
-  it('should return true', function() {
-    expect(true).to.equal(true);
+    newCustomer = new Customer(allCustomers.allCustomers[1], allBookings);
+    newCustomer2 = new Customer(allCustomers.allCustomers[2], allBookings);
+  })
+
+  it('should be a function', function() {
+    expect(Customer).to.be.a('function');
   });
+
+  it('should be an instance of Customer', () => {
+    expect(newCustomer).to.be.an.instanceOf(Customer)
+  })
+
+  it('should hold all Customer inforomation', () => {
+    expect(newCustomer.name).to.equal("Rocio Schuster");
+    expect(newCustomer.id).to.equal(2);
+    expect(newCustomer.id).to.be.a("number");
+    expect(newCustomer.allBookings).to.equal(allBookings);
+    expect(newCustomer.username).to.equal("customer2");
+    expect(newCustomer.password).to.equal("overlook2021");
+
+    expect(newCustomer2.name).to.equal("Kelvin Schiller");
+    expect(newCustomer2.id).to.equal(3);
+    expect(newCustomer2.id).to.be.a("number");
+    expect(newCustomer2.allBookings).to.equal(allBookings);
+    expect(newCustomer2.username).to.equal("customer3");
+    expect(newCustomer2.password).to.equal("overlook2021");
+  })
+
+  it('should update my bookings, number of bookings, and total spent', () => {
+    newCustomer.updateMyBookings()
+    expect(newCustomer.myBookings[0].roomNumber).to.equal(17);
+    expect(newCustomer.totalSpent).to.equal("7557.95");
+    expect(newCustomer.numBookings).to.equal(20);
+
+    newCustomer2.updateMyBookings()
+    expect(newCustomer2.myBookings[0].roomNumber).to.equal(24);
+    expect(newCustomer2.totalSpent).to.equal("4204.39");
+    expect(newCustomer2.numBookings).to.equal(12);
+  })
 });
