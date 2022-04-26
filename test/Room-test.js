41dc3ba4aa2a575c034d846407a0fa2655714e94
Ross Landino <mrlandino@gmail.com>
2022-04-26 14:24:41 -0600

    Complete room testing

--- test/Room-test.js
+++ test/Room-test.js
@@ -6,17 +6,40 @@ import BookingsRepo from '../src/BookingsRepo';
 
 
 describe('Room Class', function() {
+  let data, allBookings, newRoom
 
   beforeEach(() => {
+    data = sampleData;
+    allBookings = new BookingsRepo(data, data);
+    newRoom = new Room(allBookings.allRoomsMaster[0]);
+  })
 
-    // let data = sampleData;
-    // // console.log(sampleData)
-    // let allBookings = new BookingsRepo(data, data)
-    // let newRoom = new Room();
-    // // console.log(newRoom)
-})
-
-  it('should return true', function() {
-    expect(true).to.equal(true);
+  it('should be a function', function() {
+    expect(Room).to.be.a('function');
   });
+
+  it('should be an instance of Room', () => {
+    expect(newRoom).to.be.an.instanceOf(Room)
+  })
+
+  it('should hold all room inforomation', () => {
+    expect(newRoom.roomNumber).to.equal(1)
+    expect(newRoom.roomNumber).to.be.a("number")
+
+    expect(newRoom.roomCost).to.equal(358.4)
+    expect(newRoom.roomCost).to.be.a("number")
+
+    expect(newRoom.bedSize).to.equal("queen")
+    expect(newRoom.bedSize).to.be.a("string")
+
+    expect(newRoom.bidet).to.equal(true)
+    expect(newRoom.bidet).to.be.a("boolean")
+
+    expect(newRoom.numBeds).to.equal(1)
+    expect(newRoom.numBeds).to.be.a("number")
+
+    expect(newRoom.roomType).to.equal("residential suite")
+    expect(newRoom.roomType).to.be.a("string")
+  })
+
 });
